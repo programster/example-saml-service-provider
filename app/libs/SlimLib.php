9d05daed39a2a112b63b3f6e08055394ea9d8b0c
Programster <sdpagent@gmail.com>
2022-05-12 16:58:08 +0100

    adds helper function for returning an xml response

--- app/libs/SlimLib.php
+++ app/libs/SlimLib.php
@@ -8,10 +8,19 @@
 
 class SlimLib
 {
-    public static function createHtmlResponse(string|stringabel $content) : Psr\Http\Message\ResponseInterface
+    public static function createHtmlResponse(string|Stringable $content) : Psr\Http\Message\ResponseInterface
     {
         $response = new \Slim\Psr7\Response(200);
         $response->getBody()->write((string)$content);
+        return $response;
+    }
+
+
+    public static function createXmlResponse(string | Stringable $content) : Psr\Http\Message\ResponseInterface
+    {
+        $response = new \Slim\Psr7\Response(200);
+        $response->getBody()->write((string)$content);
+        $response = $response->withHeader("Content-Type", "text/xml");
         return $response;
     }
 }
